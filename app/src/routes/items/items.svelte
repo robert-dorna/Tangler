7ca1671318983f01c74044953023a2be1f513cce
Robert Dorna <rdorna8@gmail.com>
2023-03-23 11:06:26 +0100

    refactor: remote old debug statement

--- app/src/routes/items/items.svelte
+++ app/src/routes/items/items.svelte
@@ -1,13 +1,12 @@
 <script>
   import ItemTree from "./item-tree.svelte";
-  import { displayConfigAvailable, newItem } from "../stores";
+  import { displayConfigAvailable } from "../stores";
 
   export let items = [];
 </script>
 
 <div class="container">
   <div class="items">
-    {JSON.stringify($newItem)}
     {#if $displayConfigAvailable}
       {#each items as item, index (item._what + item._id)}
         <ItemTree {item} on:refresh />
