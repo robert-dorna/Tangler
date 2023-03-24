a9ee941fa64f167d511a60ef1258c1fd5a0fb1db
Robert Dorna <rdorna8@gmail.com>
2023-03-24 15:43:54 +0100

    fix: add missing host and port to flask dev server

--- tangle.py
+++ tangle.py
@@ -6,6 +6,6 @@ from src.server import app
 
 if __name__ == "__main__":
     if len(argv) > 1 and argv[1] == 'server':
-        app.run()
+        app.run(host='0.0.0.0', port=8000)
     else:
         run_command(**parse_args(argv[1:]))
