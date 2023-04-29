f45a425a14dad39dcef6f2b46cdeaaaa11b59d37
Robert Dorna <rdorna8@gmail.com>
2023-04-29 13:53:12 +0200

    refactor: put server logging config in separate file

--- src/server.py
+++ src/server.py
@@ -6,22 +6,10 @@ from os import path, listdir
 from werkzeug.exceptions import BadRequest
 import json
 
+import server_stuff.logging
 
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
+
+dictConfig(server_stuff.logging.config)
 
 app = Flask(__name__)
 
