2595ea63da5a5559b858d0e9d1b0454cbdb52e1d
Robert Dorna <rdorna8@gmail.com>
2023-05-24 16:22:36 +0200

    refactor: do not import cli code when running server

--- app/tangler.py
+++ app/tangler.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from sys import argv
-from src.cli import parse_args, run_command
 from src.server import run, app
 
 
@@ -8,4 +7,5 @@ if __name__ == "__main__":
     if len(argv) > 1 and argv[1] == 'server':
         run()
     else:
+        from src.cli import parse_args, run_command
         run_command(**parse_args(argv[1:]))
